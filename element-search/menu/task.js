f5366a19bac59f7765ce7348bf48b051a3524640
uri752 <uri752blag@mail.ru>
2023-01-06 13:25:39 +0300

    fix error

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,9 +2,14 @@ const allElementsMenuLinkByClassName = document.getElementsByClassName('menu__li
 for ( i = 0; i < allElementsMenuLinkByClassName.length; i++ ) {
     let elementParent = allElementsMenuLinkByClassName[i].closest('.menu__item');    
     let elementSubMenu = elementParent.querySelector('.menu_sub');    
-    if (elementSubMenu != null) {            
+    if (elementSubMenu != null) {         
         allElementsMenuLinkByClassName[i].onclick = () =>  {
-            elementSubMenu.classList.add('menu_active');            
+            
+            //elementSubMenu.classList.add('menu_active');            
+            
+            // ДОРАБОТКА - чтобы открытые меню закрывались, используем метод toggle вместо add
+            elementSubMenu.classList.toggle('menu_active');            
+            
             return false;
         }
     }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,21 +1,25 @@
-let curSlide = 0;
+
+// ДОРАБОТКА - заменить глобальную переменную локальной 
 
 const elSliderPrev = document.getElementsByClassName('slider__arrow_prev')[0];
+
+function isActiveSlide(element, index, array) {
+    return element.classList.contains('slider__item_active')
+}
+
 elSliderPrev.onclick = () => {
-    //console.log('elSliderPrev: '+elSliderPrev)
-    arraySliders[curSlide].classList.remove('slider__item_active');
-    curSlide -= 1;
-    if (curSlide < 0) { curSlide = arraySliders.length-1;}
-    arraySliders[curSlide].classList.add('slider__item_active');
+    let curSlide = arraySliders.findIndex(isActiveSlide);
+    arraySliders[curSlide].classList.toggle('slider__item_active');    
+    curSlide = (curSlide == 0 ? arraySliders.length-1 : curSlide - 1);    
+    arraySliders[curSlide].classList.toggle('slider__item_active');
 };
 
 const elSliderNext = document.getElementsByClassName('slider__arrow_next')[0];
-elSliderNext.onclick = () => {
-    //console.log('elSliderNext: '+elSliderNext)
-    arraySliders[curSlide].classList.remove('slider__item_active');
-    curSlide += 1;
-    if (curSlide >= arraySliders.length) {curSlide = 0}
-    arraySliders[curSlide].classList.add('slider__item_active');
+elSliderNext.onclick = () => {    
+    let curSlide = arraySliders.findIndex(isActiveSlide);
+    arraySliders[curSlide].classList.toggle('slider__item_active');    
+    curSlide = (curSlide == arraySliders.length-1 ? 0 : curSlide + 1);    
+    arraySliders[curSlide].classList.toggle('slider__item_active');
 };
 
 const allElSlidersByClassName = document.getElementsByClassName('slider__item');
